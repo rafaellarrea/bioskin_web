07e816cb57c378d8bdf4efc6fd82e2a8be834eef
Rafael Larrea <rafa1227_g@hotmail.com>
2025-05-22 09:39:25 -0500

    chore: switch to HashRouter for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -14,28 +13,26 @@ import Contact from './pages/Contact';
 import Products from './pages/Products';
 import WhatsAppButton from './components/WhatsAppButton';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <main className="mt-24">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/diagnosis" element={<Diagnosis />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/appointment" element={<Appointment />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </main>
-      <Footer />
-      <WhatsAppButton />
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <HashRouter>
+    <Navbar />
+    <main className="mt-24">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/diagnosis" element={<Diagnosis />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </main>
+    <Footer />
+    <WhatsAppButton />
+  </HashRouter>
+);
 
 export default App;
