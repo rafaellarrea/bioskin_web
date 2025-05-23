688abba091c13963ea41271a8333e0a4412c5e63
Rafael Larrea <rafa1227_g@hotmail.com>
2025-05-22 21:24:54 -0500

    Corrijo sintaxis style en Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,8 +5,12 @@ const Products = () => {
     <section style={{ padding: '80px', textAlign: 'center' }}>
       <h1>Products</h1>
       <p>Render test con imágenes incluidas.</p>
-      <div style={ marginTop: '40px' }}>
-        <img src="https://via.placeholder.com/300" alt="Imagen de prueba" style={{ maxWidth: "100%", height: "auto" }}} />
+      <div style={{ marginTop: '40px' }}>
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Imagen de prueba"
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
       </div>
     </section>
   );
