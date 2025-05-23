4f65baa309fcf8283e22d99136defd558edaf620
Rafael Larrea <rafa1227_g@hotmail.com>
2025-05-22 21:32:18 -0500

    Corrijo error de estilo en Contact.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,8 +5,12 @@ const About = () => {
     <section style={{ padding: '80px', textAlign: 'center' }}>
       <h1>About</h1>
       <p>Render test con imágenes incluidas.</p>
-      <div style={ marginTop: '40px' }}>
-        <img src="https://via.placeholder.com/300" alt="Imagen de prueba" style={{ maxWidth: "100%", height: "auto" }}} />
+      <div style={{ marginTop: '40px' }}>
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Imagen de prueba"
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
       </div>
     </section>
   );
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 const Contact = () => {
   return (
     <section style={{ padding: '80px', textAlign: 'center' }}>
-      <h1>Contact</h1>
+      <h1>Contacto</h1>
       <p>Render test con imágenes incluidas.</p>
-      <div style={ marginTop: '40px' }}>
-        
+      <div style={{ marginTop: '40px' }}>
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Imagen de prueba"
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
       </div>
     </section>
   );
