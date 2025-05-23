7bf3e873ddfed71a1b9fcace9044158f044c440d
Rafael Larrea <rafa1227_g@hotmail.com>
2025-05-22 21:34:54 -0500

    Corrijo error de estilo en Faq.tsx

--- src/pages/Diagnosis.tsx
+++ src/pages/Diagnosis.tsx
@@ -5,8 +5,12 @@ const Diagnosis = () => {
     <section style={{ padding: '80px', textAlign: 'center' }}>
       <h1>Diagnosis</h1>
       <p>Render test con imágenes incluidas.</p>
-      <div style={ marginTop: '40px' }}>
-        <img src="https://via.placeholder.com/300" alt="Imagen de prueba" style={{ maxWidth: "100%", height: "auto" }}} />
+      <div style={{ marginTop: '40px' }}>
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Imagen de prueba"
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
       </div>
     </section>
   );
--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 const Faq = () => {
   return (
     <section style={{ padding: '80px', textAlign: 'center' }}>
-      <h1>Faq</h1>
+      <h1>FAQ</h1>
       <p>Render test con imágenes incluidas.</p>
-      <div style={ marginTop: '40px' }}>
-        
+      <div style={{ marginTop: '40px' }}>
+        <img
+          src="https://via.placeholder.com/300"
+          alt="Imagen de prueba"
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
       </div>
     </section>
   );
