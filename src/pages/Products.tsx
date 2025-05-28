fa8dba2725dc01b4af40d4a4e6f8645c95be7ccc
Rafael Larrea <rafa1227_g@hotmail.com>
2025-05-28 16:31:24 -0500

    correcion products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -9,7 +9,7 @@ const Products = () => {
   const products = [
     {
       name: "ANALIZADOR FACIAL INTELIGENTE CON PANTALLA DE 21 pulgadas",
-      description: "Experiencia de análisis de piel profesional con tecnología de 	vanguardia: esta máquina analizadora de piel inteligente Bitmoji 3D AI utiliza 	tecnología RGB+UV+PL para proporcionar un análisis de piel integral. Detecta y 	analiza diversas afecciones de la piel, como acné, arrugas, poros, manchas y 	niveles de hidratación, proporcionando informes detallados para el cuidado 	personalizado de la piel.",
+      description: "Experiencia de análisis de piel profesional con tecnología de vanguardia: esta máquina analizadora de piel inteligente 	Bitmoji 3D AI utiliza 	tecnología RGB+UV+PL para proporcionar un análisis de piel integral. Detecta y 	analiza diversas 	afecciones de la piel, como acné, arrugas, poros, manchas y 	niveles de hidratación, proporcionando informes detallados para el 	cuidado personalizado de la piel.",
       price: 3100,
       images: [
         "/images/productos/dispositivos/analizador/analizador1.jpg",
