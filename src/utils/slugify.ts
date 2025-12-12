9db83f89409957d8fb8c7ea0f7af97dd4b221250
RAFA <rafa1227_g@hotmail.com>
2025-12-12 18:18:22 -0500

    Fix: Corrección de generación de slugs para productos con caracteres especiales (tildes, subíndices)

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,7 +1,13 @@
 // src/utils/slugify.ts
 
 export function slugify(text: string) {
-  return text.toLowerCase()
+  return text
+    .toString()
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/₂/g, '2')
     .replace(/[^\w\s-]/g, '')
+    .trim()
     .replace(/\s+/g, '-');
 }
